4aeb29d32e26df4ab9e5db983d9eacb96f502690
Karipalli-Reddy-Merson <merson1717@gmail.com>
2025-04-03 21:24:53 +0530

    modified

--- inputs.js
+++ inputs.js
@@ -10,9 +10,9 @@ function Submit(){
  let branch =document.getElementById("demo3");
  console.log("Branch:"+branch[0].value);
  let gender =document.getElementById("demo4");
- console.log(gender.name);
+ console.log("Gender:"+gender.value);
  let language =document.getElementById("demo5");
- console.log(language.name);
+ console.log("Language:"+language.value);
  let phoneno =document.getElementById("demo6");
  console.log("Mobile:"+phoneno.value);
  let dob = document.getElementById("dob");
