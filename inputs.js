072c3278ab209fca6d8d634f9a54d99d5e1c05fb
Karipalli-Reddy-Merson <merson1717@gmail.com>
2025-04-03 21:11:34 +0530

    modified

--- inputs.js
+++ inputs.js
@@ -2,17 +2,20 @@ function Submit(){
  let input =document.getElementsByTagName("input");
  console.log(input[0].value);
  let name= document.getElementById("demo");
- console.log(name.value);
+ console.log("Name:"+name.value);
  let email =document.getElementById("demo1");
- console.log(email.value);
+ console.log("Email:"+email.value);
  let qualification =document.getElementById("demo2");
- console.log(qualification[0].value);
+ console.log("Qualification:"+qualification[0].value);
  let branch =document.getElementById("demo3");
- console.log(branch[0].value);
+ console.log("Branch:"+branch[0].value);
  let gender =document.getElementById("demo4");
  console.log(gender.name);
  let language =document.getElementById("demo5");
  console.log(language.name);
  let phoneno =document.getElementById("demo6");
- console.log(phoneno.value);
+ console.log("Mobile:"+phoneno.value);
+ let dob = document.getElementById("dob");
+ console.log("Date of Birth: " + dob.value);
+ 
 }
