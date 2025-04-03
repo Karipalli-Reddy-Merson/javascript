e1c23a835c70620bda6a81a61bc598294541acfa
Karipalli-Reddy-Merson <merson1717@gmail.com>
2025-04-03 19:48:26 +0530

    add file

---
+++ inputs.js
@@ -0,0 +1,18 @@
+function Submit(){
+ let input =document.getElementsByTagName("input");
+ console.log(input[0].value);
+ let name= document.getElementById("demo");
+ console.log(name.value);
+ let email =document.getElementById("demo1");
+ console.log(email.value);
+ let qualification =document.getElementById("demo2");
+ console.log(qualification[0].value);
+ let branch =document.getElementById("demo3");
+ console.log(branch[0].value);
+ let gender =document.getElementById("demo4");
+ console.log(gender.name);
+ let language =document.getElementById("demo5");
+ console.log(language.name);
+ let phoneno =document.getElementById("demo6");
+ console.log(phoneno.value);
+}
